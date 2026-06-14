f27c2baf68f9e7eb271490fef5509bdc506d41ee
Ushan02 <ushansanuka2023@gmail.com>
2026-06-14 09:38:34 +0530

    splash page update

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 import { useEffect } from "react";
-import { ActivityIndicator, View } from "react-native";
-import { theme } from "../constants/theme";
+import { ImageBackground, StyleSheet } from "react-native";
 
 export default function Index() {
   useEffect(() => {
@@ -17,15 +16,19 @@ export default function Index() {
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: theme.colors.bg,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ActivityIndicator size="large" color={theme.colors.primary} />
-    </View>
+    <ImageBackground
+      source={require("../assets/images/splash.jpg")}
+      style={styles.splash}
+      resizeMode="cover"
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  splash: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#F5F5F0",
+  },
+});
